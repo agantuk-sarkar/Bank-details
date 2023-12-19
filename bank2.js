16d980282ca765d018e97e0c1037630f7185b90e
Agantuk Sarkar <agantuksarkar124@gmail.com>
2023-12-20 03:02:41 +0530

    Updated contents

--- bank2.js
+++ bank2.js
@@ -33,6 +33,9 @@ userForm.addEventListener("submit", function(event){
   arr.push(userObj);
   localStorage.setItem("myBank", JSON.stringify(arr));
 
+  // display/calling the function
+  display(arr);
+
   // console.log(userObj);
   // console.log(arr);
 })
@@ -58,6 +61,7 @@ depositForm.addEventListener("submit",function(event){
       // console.log(depositUserElement);
       // console.log(arr);
       localStorage.setItem("myBank",JSON.stringify(arr));
+      display(arr);
     } else{
       alert("Enter valid Account Number");
     }
@@ -84,8 +88,37 @@ withdrawForm.addEventListener("submit",function(event){
     withdrawUserElement.withdraw(+amountToWithdraw);
     // console.log(arr);
     localStorage.setItem("myBank",JSON.stringify(arr));
+    display(arr);
   } else{
     alert("Enter valid Account Number");
   }
 })
+
+// Creating contents for table
+
+function display(array){
+  let tbody = document.querySelector("tbody");
+  tbody.textContent = "";
+
+  // Mapping the array
+  array.map(function(ele,index){
+
+    let row = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.textContent = index + 1;
+
+    let td2 = document.createElement("td");
+    td2.textContent = ele.name;
+
+    let td3 = document.createElement("td");
+    td3.textContent = ele.accountNumber;
+
+    let td4 = document.createElement("td");
+    td4.textContent = ele.balance;
+
+    row.append(td1,td2,td3,td4);
+    tbody.append(row);
+  })
+}
 
