463a3b99a8b811ddcc37779b4c77cf57d1622d86
Agantuk Sarkar <agantuksarkar124@gmail.com>
2023-12-21 02:09:19 +0530

    Updated contents

--- bank2.js
+++ bank2.js
@@ -1,6 +1,7 @@
 // Constructor function
 function Bank(name,customerBalance=0){
     this.name = name;
+    this.date = new Date();
     this.accountNumber = Date.now();
     this.balance = customerBalance;
 }
@@ -12,6 +13,8 @@ function Bank(name,customerBalance=0){
   // Bank.prototype.withdraw = function (amountToWithdraw) {
   //   return (this.balance = this.balance - amountToWithdraw);
   // };
+
+  //Encapsulationg into one object to be used as Prototype 
   let setPrototype = {
     deposit: function(amountToDeposit){
       return (this.balance = this.balance + amountToDeposit);
@@ -20,6 +23,11 @@ function Bank(name,customerBalance=0){
       return (this.balance = this.balance - amountToWithdraw);
     },
   };
+
+  // Function for date format
+  function dateFormat(){
+    this.date = this.date.toISOString().split("T")[0];
+  }
 
 // Create account of the user
 let arr = JSON.parse(localStorage.getItem("myBank")) || [];
@@ -30,6 +38,10 @@ userForm.addEventListener("submit", function(event){
   let customerName = document.querySelector("#name").value;
   let customerAmount = document.querySelector("#amount").value;
   let userObj = new Bank(customerName, +customerAmount);
+
+  // calling the dateFormat function by call() method
+  dateFormat.call(userObj);
+
   arr.push(userObj);
   localStorage.setItem("myBank", JSON.stringify(arr));
 
@@ -100,7 +112,7 @@ function display(array){
   let tbody = document.querySelector("tbody");
   tbody.textContent = "";
 
-  // Mapping the array
+  // Mapping the array using higher order function
   array.map(function(ele,index){
 
     let row = document.createElement("tr");
@@ -112,13 +124,31 @@ function display(array){
     td2.textContent = ele.name;
 
     let td3 = document.createElement("td");
-    td3.textContent = ele.accountNumber;
+    td3.textContent = ele.date;
 
     let td4 = document.createElement("td");
-    td4.textContent = ele.balance;
+    td4.textContent = ele.accountNumber;
 
-    row.append(td1,td2,td3,td4);
+    let td5 = document.createElement("td");
+    td5.textContent = ele.balance;
+
+    let td6 = document.createElement("td");
+    td6.textContent = "Delete";
+    td6.style.backgroundColor = "red";
+    td6.style.cursor = "pointer";
+    td6.addEventListener("click",function(){
+      deleteRow(index);
+    })
+
+    row.append(td1,td2,td3,td4,td5,td6);
     tbody.append(row);
   })
+};
+
+// Creating delete row function
+function deleteRow(ind){
+  arr.splice(ind,1);
+  localStorage.setItem("myBank",JSON.stringify(arr));
+  display(arr);
 }
 
